7a0335e03ee171007482c953455a3f89811044a5
SirojiddinCoder <SirojiddinCoder9998@gmail.com>
2021-11-14 08:30:04 -0800

    test3

--- js.js
+++ js.js
@@ -1,21 +1,35 @@
-// function Person(first, last, age, eye) {
-//     this.firstName = first;
-//     this.lastName = last;
-//     this.age = age;
-//     this.eyeColor = eye;
+// // function Person(first, last, age, eye) {
+// //     this.firstName = first;
+// //     this.lastName = last;
+// //     this.age = age;
+// //     this.eyeColor = eye;
+// //   }
+
+
+// //   const myFather = new Person("Eshmat", "Toshmat", 25, "blue");
+
+
+// //   console.log( "My friend is " + myFather.age + ".");
+
+// class Car {
+//     constructor(name, year) {
+//       this.name = name;
+//       this.year = year;
+//     }
 //   }
+//   const myCar = new Car("Cobalt", 2021);
+// console.log(myCar.name + ' ' + myCar.year);
 
-
-//   const myFather = new Person("Eshmat", "Toshmat", 25, "blue");
-
-
-//   console.log( "My friend is " + myFather.age + ".");
 
 class Car {
-    constructor(name, year) {
+    constructor(name) {
       this.name = name;
-      this.year = year;
+    }
+    static hello() {
+      return "WebBrain Academy";
     }
   }
-  const myCar = new Car("Cobalt", 2021);
-console.log(myCar.name + ' ' + myCar.year);
+
+  let myCar = new Car("Cobalt");
+
+  console.log(Car.hello());
